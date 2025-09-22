1262cb2c1b34fc37d5367b0c74f36421f10f662a
Rudiansyah <rudiansyahether@gmail.com>
2025-09-22 14:17:36 +0000

    part 1 add openweather api

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::env;
 use std::error::Error;
 
+/*
+https://api.openweathermap.org/data/2.5/weather?lat=-0.2252806&lon=109.6446236&appid=env
+*/
+
 fn main() -> Result<(), Box<dyn Error>> {
+    let api_key = env::var("MY_WEATHER_API_KEY")
+        .expect("Please set the MY_WEATHER_API_KEY environment variable");
+
     let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
         eprintln!("Usage: weather_api_cli <latitude> <longitude>");
@@ -9,6 +16,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     let lat: &String = &args[1];
     let long: &String = &args[1];
+
+    let url = format!(
+        "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}",
+        lat, long, api_key
+    );
 
     print!("Debug! :: {} || {}", lat, long);
     println!("Hello, world! Rust");
