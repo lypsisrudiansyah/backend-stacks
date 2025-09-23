7e45e1dd9ab697a15ad341908c10d419a0e24599
Rudiansyah <rudiansyahether@gmail.com>
2025-09-23 14:16:59 +0000

    index

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+function fizzBuzz(value: number): string | number {
+    if (value % 15 == 0) {
+        return "FizzBuzz";
+    } else if (value % 5 == 0) {
+        return "Buzz";
+    } else if (value % 3 == 0) {
+        return "Fizz";
+    } else {
+        return value;
+    }
+}
+for (let i = 0; i < 30; i++) {
+    let output: string | number = fizzBuzz(i);
+    console.log("The Output : ", output);
+}
