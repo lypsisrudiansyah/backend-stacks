ffc57199afef7fdc7409538b1c869f2f576a0366
Rudiansyah <rudiansyahether@gmail.com>
2025-09-23 15:30:27 +0000

    update server

--- src/server.ts
+++ src/server.ts
@@ -15,8 +15,37 @@ const theServer = http.createServer((req, res) => {
             res.end(data);
         });
     } else if (req.url === "/client.js" && req.method === "GET") {
-
+        fs.readFile(path.join(__dirname, "client.js"), "utf-8", (err, data) => {
+            if (err) {
+                res.writeHead(500, { "Content-Type": "text/plain" })
+                res.end("Internal Server Error")
+                return;
+            }
+            res.writeHead(200, { "Content-Type": "text/javascript" })
+            res.end(data);
+        });
     }
 });
+
+const wss = new WebSocketServer({ theServer });
+
+wss.on("connection", (ws) => {
+    console.log("Client connected!");
+
+    ws.on("message", (message: Buffer) => {
+        console.log("Received: ", message.toString())
+
+        ws.send(`Echo: ${message.toString()}`);
+    })
+
+    ws.on("close", () => {
+        console.log("Client disconnected");
+    })
+    ws.on("error", (error: any) => {
+        console.error("WebSocket error:", error);
+    })
+});
+
+
 
 
