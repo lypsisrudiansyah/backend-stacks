45a2a4e36cd1882ca58e40a8e2a5003ddcb59fbc
Rudiansyah <rudiansyahether@gmail.com>
2025-09-23 14:34:40 +0000

    update serverts

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,16 @@ import { WebSocketServer } from "ws";
 const theServer = http.createServer((req, res) => {
     if (req.url === "/" && req.method == "GET") {
         fs.readFile(path.join(__dirname, "index.html"), "utf-8", (err, data) => {
-
+            if (err) {
+                res.writeHead(500, { "Content-Type": "text/plain" });
+                res.end("Internal Server Error");
+                return;
+            }
+            res.writeHead(200, { "Content-Type": "text/html" })
+            res.end(data);
         });
+    } else if (req.url === "/client.js" && req.method === "GET") {
+
     }
 });
 
